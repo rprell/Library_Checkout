7087b2784a251035661a70fae09a0d1c01131e6b
dinajdadollani <ddollani24@scienceleadership.org>
2023-03-22 08:50:55 -0400

    ..

--- src/main/java/com/example/library_checkout/HelloApplication.java
+++ src/main/java/com/example/library_checkout/HelloApplication.java
@@ -8,13 +8,21 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    HelloController myController;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        myController = fxmlLoader.getController();
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void stop ()throws Exception{
+        myController.saveData();
     }
 
     public static void main(String[] args) {
